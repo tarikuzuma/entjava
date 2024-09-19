bb0983188f580c8345997f6c71964ee276280206
Danziel Cempron <104302220+Dnzldotexe@users.noreply.github.com>
2024-09-19 22:25:01 +0800

    Regex for Email
    bro this is my first pull request on this repo bro, please accept it bro, I would blip myself out of existence if this doesn't get accepted bro, please bro

--- src/main/java/com/example/midterm/MyController.java
+++ src/main/java/com/example/midterm/MyController.java
@@ -6,19 +6,23 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @RestController
 @RequestMapping("validate")
 public class MyController {
+    public static final Pattern VALID_EMAIL_ADDRESS_REGEX =
+            Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
 
     @GetMapping("/email/{email}")
     public boolean validateEmail(@PathVariable String email) {
-        return true;
+        Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(email);
+        return matcher.matches();
     }
-
+    
     @GetMapping("/name/{name}")
     public Name processName(@PathVariable String name) {
         return new Name();
     }
-
-
 }
