73bfa4710e3ef639358c614ab827f7ff85ba6555
ztest95 <110767420+ztest95@users.noreply.github.com>
2024-09-19 23:52:16 +0800

    Edit email validation endpoint

--- src/main/java/com/example/midterm/ValidateController.java
+++ src/main/java/com/example/midterm/ValidateController.java
@@ -1,9 +1,9 @@
 package com.example.midterm;
 
-import dto.Name;
+
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.regex.Matcher;
@@ -11,12 +11,12 @@ import java.util.regex.Pattern;
 
 @RestController
 @RequestMapping("validate")
-public class MyController {
+public class ValidateController {
     public static final Pattern VALID_EMAIL_ADDRESS_REGEX =
             Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
 
-    @GetMapping("/email/{email}")
-    public boolean validateEmail(@PathVariable String email) {
+    @GetMapping("/email")
+    public boolean validateEmail(@RequestParam String email) {
         Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(email);
         return matcher.matches();
     }
