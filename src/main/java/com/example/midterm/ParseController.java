e1f75e3f6626a86a5a9426509e4d53b09698fca8
ztest95 <110767420+ztest95@users.noreply.github.com>
2024-09-21 06:35:34 +0800

    add sample implementation

--- src/main/java/com/example/midterm/ParseController.java
+++ src/main/java/com/example/midterm/ParseController.java
@@ -2,6 +2,7 @@ package com.example.midterm;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import java.util.Map;
 @RequestMapping("parse")
 public class ParseController {
     
+    @CrossOrigin(origins = "*")
     @GetMapping("/name")
     public ResponseEntity<?> processName(@RequestParam String name) {
         NameService nameService = new ConcreteNameService();
--- src/main/java/com/example/midterm/ValidateController.java
+++ src/main/java/com/example/midterm/ValidateController.java
@@ -1,6 +1,7 @@
 package com.example.midterm;
 
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,6 +16,7 @@ public class ValidateController {
     public static final Pattern VALID_EMAIL_ADDRESS_REGEX =
             Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
 
+    @CrossOrigin(origins = "*")
     @GetMapping("/email")
     public boolean validateEmail(@RequestParam String email) {
         Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(email);
